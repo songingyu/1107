0d6eee467218e77591239282edbb3f0e97b64d5c
songingyu <55523987+songingyu@users.noreply.github.com>
2019-11-07 15:18:15 +0900

    Add files via upload

---
+++ exam01.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void stars();
+int main()
+{
+	print_stars();
+	printf("Hello C world\n");
+	print_stars();
+	
+	return 0;
+}
+
+void print_stars()
+{
+	printf("*********************\n");
+}
